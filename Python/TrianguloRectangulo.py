49310e90a3c22e81807cdab108037a01dda77df0
Danielherran <143098662+Danielherran@users.noreply.github.com>
2023-11-10 22:32:24 -0500

    Update TrianguloRectangulo.py

--- Python/TrianguloRectangulo.py
+++ Python/TrianguloRectangulo.py
@@ -4,4 +4,19 @@ class TrianguloRectangulo:
   def _Init_ (self, diametro, angulo):
     self.diametro = diaetro
     self.angulo = angulo
-    
+
+  def catetoA (self):
+    self.catetoA = math.sin(self.angulo)
+    return self.catetoA
+  def CatetoB (self):  
+    self.catetob = math.cos(self.angulo)
+    return self.catetoA
+
+  def area (self, catetoA, catetoB):
+    self.area = (catetoA*catetoB)/2
+    return self area
+
+x = input ("ingrese la hipotenusa del triangulo: ")
+y =input("ingrese el angulo del triangulo: ") 
+tri = TrianguloRectangulo(int(x), int(y))
+print("el area del triangulo es:", tri.area())
